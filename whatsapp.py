03ad437f912998ce9ca997ee2a9f9e9e5bef64c6
ToniIvars <toniivaju@gmail.com>
2021-08-09 09:18:16 +0200

    Added a new method and returns, for the errors and when the method worked well

--- whatsapp.py
+++ whatsapp.py
@@ -26,15 +26,33 @@ class WhatsappAutomation:
         try:
             WebDriverWait(self.driver, 20).until(lambda s:s.find_element_by_id('side'))
             self.driver.minimize_window()
+
+            self.inp = WebDriverWait(self.driver, 10).until(lambda s:s.find_element_by_css_selector('._1LbR4 > div:nth-child(2)'))
+
         except TimeoutException:
             self.quit()
             print('You have 20 seconds to read the QR code.')
 
     def one_message(self, message, times=1):
-        inp = WebDriverWait(self.driver, 10).until(lambda s:s.find_element_by_css_selector('._1LbR4 > div:nth-child(2)'))
+        if not message:
+            return 'Write a message'
+        if times < 1:
+            return 'You cannot send a messages less than 1 time'
+
         for _ in range(times):
-            inp.send_keys(message + Keys.ENTER)
+            self.inp.send_keys(message + Keys.ENTER)
             time.sleep(0.8)
+
+        return f'Message sent successfully {times} time{"s" if times > 1 else ""}'
+
+    def many_messages(self, messages_list):
+        if not messages_list:
+            return 'Pass a list of messages'
+        for message in messages_list:
+            self.inp.send_keys(message + Keys.ENTER)
+            time.sleep(0.8)
+
+        return f'{len(messages_list)} messages sent successfully'
 
     def quit(self):
         self.driver.quit()
@@ -47,4 +65,4 @@ if __name__ == '__main__':
     atexit.register(w.quit)
 
     message = input('Insert the message you want to send: ')
-    w.one_message(message)
+    print('\n' + w.one_message(message))
