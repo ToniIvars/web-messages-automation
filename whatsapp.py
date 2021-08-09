8c5cc14de21ff75fde8e4ba3ccfad3d6d196ae46
ToniIvars <toniivaju@gmail.com>
2021-08-09 08:55:46 +0200

    Added some exception handling and a pause between message sendings

--- whatsapp.py
+++ whatsapp.py
@@ -1,7 +1,9 @@
 from selenium import webdriver
 from selenium.webdriver.support.ui import WebDriverWait
 from selenium.webdriver.common.keys import Keys
+from selenium.common.exceptions import TimeoutException
 import phonenumbers
+import time
 
 class InvalidPhoneError(Exception):
     def __init__(self):
@@ -11,7 +13,9 @@ class InvalidPhoneError(Exception):
 class WhatsappAutomation:
     def __init__(self, phone):
         try:
-            parsed_phone = phonenumbers.parse(phone, None)
+            parsed_phone = phonenumbers.parse(phone, 'ES')
+            if not phonenumbers.is_valid_number(parsed_phone):
+                raise InvalidPhoneError
         except:
             raise InvalidPhoneError
 
@@ -19,14 +23,18 @@ class WhatsappAutomation:
         self.driver.get(f'https://web.whatsapp.com/send?phone={phone}')
 
         print('Read the QR code with your phone, please (this will only occur once).')
-
-        WebDriverWait(self.driver, 20).until(lambda s:s.find_element_by_id('side'))
-        self.driver.minimize_window()
+        try:
+            WebDriverWait(self.driver, 20).until(lambda s:s.find_element_by_id('side'))
+            self.driver.minimize_window()
+        except TimeoutException:
+            self.quit()
+            print('You have 20 seconds to read the QR code.')
 
     def one_message(self, message, times=1):
         inp = WebDriverWait(self.driver, 10).until(lambda s:s.find_element_by_css_selector('._1LbR4 > div:nth-child(2)'))
         for _ in range(times):
             inp.send_keys(message + Keys.ENTER)
+            time.sleep(0.8)
 
     def quit(self):
         self.driver.quit()
@@ -36,5 +44,7 @@ if __name__ == '__main__':
 
     phone = input('Insert the phone number with the county code: ')
     w = WhatsappAutomation(phone)
-
     atexit.register(w.quit)
+
+    message = input('Insert the message you want to send: ')
+    w.one_message(message)
