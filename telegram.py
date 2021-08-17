6ae57c8e8370945b43329eb45d9771e985426d72
ToniIvars <toniivaju@gmail.com>
2021-08-17 16:32:00 +0200

    Added a method for selecting the recipient of a message and modified the log-in part to catch errors

--- telegram.py
+++ telegram.py
@@ -25,27 +25,63 @@ class TelegramAutomation:
         else:
             phone_input.send_keys(phone)
 
-        time.sleep(2)
+        time.sleep(1)
         self.driver.find_element_by_css_selector('button.Button:nth-child(4)').click()
 
-        code_input = WebDriverWait(self.driver, 5).until(lambda s: s.find_element_by_id('sign-in-code'))
+        try:
+            code_input = WebDriverWait(self.driver, 5).until(lambda s: s.find_element_by_id('sign-in-code'))
+        except TimeoutException:
+            try:
+                self.driver.find_element_by_class_name('error')
+                print('There was an error with the phone.')
+            except:
+                print('There was an unknown error.')
+            finally:
+                self.quit()
 
-        code = input('Enter the verification code sent to the Telegram app on your phone: ')
-        code_input.send_keys(code)
+        while True:
+            code = input('Enter the verification code sent to the Telegram app on your phone: ')
+            code_input.send_keys(code)
+
+            try:
+                WebDriverWait(self.driver, 2).until(lambda s: s.find_element_by_class_name('error'))
+                print('The code is incorrect.')
+                code_input.clear()
+
+            except TimeoutException:
+                break
 
         try:
             password_input = WebDriverWait(self.driver, 5).until(lambda s: s.find_element_by_id('sign-in-password'))
 
-            password = getpass('Enter the 2FA password: ')
-            password_input.send_keys(password)
+            while True:
+                password = getpass('Enter the 2FA password: ')
+                password_input.send_keys(password)
 
-            time.sleep(2)
-            self.driver.find_element_by_css_selector('.Button').click()
+                time.sleep(1)
+                self.driver.find_element_by_css_selector('.Button').click()
+
+                try:
+                    WebDriverWait(self.driver, 2).until(lambda s: s.find_element_by_class_name('error'))
+                    print('The password is incorrect.')
+                    password_input.clear()
+
+                except TimeoutException:
+                    break
 
         except TimeoutException:
             pass
 
         self.search_bar = WebDriverWait(self.driver, 10).until(lambda s: s.find_element_by_id('telegram-search-input'))
+
+    def change_recipient(self, name):
+        self.search_bar.click()
+        time.sleep(1)
+        self.search_bar.send_keys(name)
+
+        WebDriverWait(self.driver, 5).until(lambda s: s.find_element_by_class_name('search-section'))
+
+        self.search_bar.send_keys(Keys.ENTER)
 
     def quit(self):
         print('Exiting...')
@@ -55,5 +91,8 @@ class TelegramAutomation:
 if __name__ == '__main__':
     phone = input('Insert the phone number for logging in: ')
     t = TelegramAutomation(phone)
+
+    recipient = input('Recipient of the message: ')
+    t.change_recipient(recipient)
 
     t.quit()
